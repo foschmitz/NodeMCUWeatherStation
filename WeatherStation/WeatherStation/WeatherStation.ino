8f7383cd6ae8cc615c6c25c6184d7bcd4ed55e59
Frank Schmitz <frank.schmitz@flowfact.de>
2018-03-16 10:57:09 +0100

    Initial Commit

---
+++ WeatherStation/WeatherStation/WeatherStation.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		WeatherStation.ino
+ Created:	16.03.2018 10:56:18
+ Author:	Frank Schmitz
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
